1ae15d6e23538fdcb97378d06ee9e9c290d8fdd1
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2023-07-09 10:12:56 -0300

    adiciona gráfico de pizza na tela de dashboard.

--- TarefasApp.UI/Models/DashboardViewModel.cs
+++ TarefasApp.UI/Models/DashboardViewModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TarefasApp.UI.Models
+﻿namespace TarefasApp.UI.Models
 {
-    class DashboardViewModel
+    public class DashboardViewModel
     {
+        public DashboardViewModel()
+        {
+            DashboardModelList = new List<DadosDashboardModel>
+            {
+                new DadosDashboardModel { Categoria = "Trabalho", Quantidade = 15 },
+                new DadosDashboardModel { Categoria = "Estudo", Quantidade = 10 },
+                new DadosDashboardModel { Categoria = "Família", Quantidade = 5 },
+                new DadosDashboardModel { Categoria = "Outros", Quantidade = 5 }
+            };
+        }
+        public List<DadosDashboardModel> DashboardModelList { get; set; }
+    }
+
+    public class DadosDashboardModel
+    {
+        public string Categoria { get; set; }
+        public int? Quantidade { get; set; }
     }
 }
